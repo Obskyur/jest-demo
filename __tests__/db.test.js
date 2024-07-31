2cb331b1aac482f64094c338a1dabc33bcfebbb3
Tristan Miller <parkourman.miller@gmail.com>
2024-07-31 12:49:41 -0700

    rf: it -> test, user -> _user

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import User from '../models/user';
-import { user as _user } from '../fixtures/db';
+import { _user } from '../fixtures/db';
 
 const MONGO_URL = 'mongodb://localhost:27017/aroundtheus';
 const { connect, disconnect } = mongoose;
@@ -33,7 +33,7 @@ describe('Database tests', () => {
 
   afterEach(() => User.deleteOne({ email: _user.email }));
 
-  it('The user must be complete', () => {
+  test('The user must be complete', () => {
     return User.findOne({ email: _user.email })
       .then((user) => {
         expect(user).toBeDefined();
--- fixtures/db.js
+++ fixtures/db.js
@@ -1,4 +1,4 @@
-export const user = {
+export const _user = {
   name: 'Wendy Webberton',
   about: 'Super Web Development',
   password: '1234QWer$',
